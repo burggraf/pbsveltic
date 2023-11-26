8164c5006b1bce97b8c7f755cc761859373d9773
Mark Burggraf <markb@mantisbible.com>
2023-11-26 07:31:04 -0800

    login, register, reset password

---
+++ src/services/pocketbase.auth.service.ts
@@ -0,0 +1,34 @@
+import { pb } from '$services/pocketbase.service';
+import { writable } from 'svelte/store';
+
+export let currentUser: any = writable<any | null>(null);
+
+
+const removeListener1 = pb.authStore.onChange((token, model) => {
+  console.log('********* authStore changed:', token, model)
+  currentUser.set(model);
+});
+
+export const loadUser = async () => {
+  // get all keys from localStorage
+  if (pb.authStore.model) {
+    currentUser.set(pb.authStore.model);
+  }
+};
+loadUser();
+
+// supabase.auth.onAuthStateChange(async (event, session) => {
+//   currentUser.set(session?.user ?? null);
+// });  
+
+// export let currentUserId = '';
+// currentUser.subscribe((value: any) => {
+//   if (value?.id) {
+//     currentUserId = value?.id;
+//   } else {
+//     currentUserId = '';
+//   }
+// })
+
+
+
