ce65a73c8f616f613089e94fa2212d250355b7f5
Mark Burggraf <markb@mantisbible.com>
2023-12-04 18:27:17 -0800

    scores

--- src/routes/quiz/+page.svelte
+++ src/routes/quiz/+page.svelte
@@ -6,6 +6,8 @@
     import { initQuestion } from "./quiz.interfaces";
     import { currentUser } from '$services/backend.service';
     let question: Question = initQuestion();
+    let correct: number = 0;
+    let total: number = 0;
     const getQuestion = async () => {
         const result = await pb.send("/getquestion", {});
         question = result.result as Question; 
@@ -14,6 +16,26 @@
     onMount(async () => {
         await getQuestion();
     });
+    const ionViewDidEnter = async () => {
+        await getScore();
+    }
+    const getScore = async () => {
+        if (!$currentUser) {
+            console.log('*** no currentUser -- aborting getScore() ***');
+            return;
+        }
+        try {
+            const record = await pb.collection('trivia_log_score').getFirstListItem(`id="${$currentUser.id}"`, {});
+            if (record) {
+                console.log('*** getScore() record', record);
+                correct = record.correct;
+                total = record.total;
+            }
+        } catch (error) {
+            // no data, probably
+            console.log('*** getScore() error', error);
+        }
+    }
     const logQuestion = async (letter: string) => {
         if (!$currentUser) {
             console.log('*** no currentUser -- aborting logQuestion() ***');
@@ -36,6 +58,7 @@
                     chosenEl.color = 'success';
                     chosenEl.fill = 'outline';
                 }
+                correct++;
             } else {
                 if (chosenEl) {
                     chosenEl.color = 'danger';
@@ -46,6 +69,7 @@
                     correctEl.fill = 'outline';
                 }
             }
+            total++;
             logQuestion(letter);
             setTimeout(() => {
                 for (let i = 0; i < 4; i++) {
@@ -60,7 +84,7 @@
         }
     }
 </script>
-<IonPage>
+<IonPage {ionViewDidEnter}>
     <ion-header>
         <ion-toolbar>
             <ion-buttons slot="start">
@@ -82,13 +106,23 @@
             </ion-card-content>
           
             <div class="ion-padding">
-            <ion-button id="item-0" fill="clear" on:click={selectAnswer(question.answerMap[0])}>{question[question.answerMap[0]]}</ion-button>
-            <ion-button id="item-1" fill="clear" on:click={selectAnswer(question.answerMap[1])}>{question[question.answerMap[1]]}</ion-button>
-            <ion-button id="item-2" fill="clear" on:click={selectAnswer(question.answerMap[2])}>{question[question.answerMap[2]]}</ion-button>
-            <ion-button id="item-3" fill="clear" on:click={selectAnswer(question.answerMap[3])}>{question[question.answerMap[3]]}</ion-button>
+            <ion-button class="item-text-wrap" id="item-0" fill="clear" on:click={selectAnswer(question.answerMap[0])}>{question[question.answerMap[0]]}</ion-button>
+            <ion-button class="item-text-wrap" id="item-1" fill="clear" on:click={selectAnswer(question.answerMap[1])}>{question[question.answerMap[1]]}</ion-button>
+            <ion-button class="item-text-wrap" id="item-2" fill="clear" on:click={selectAnswer(question.answerMap[2])}>{question[question.answerMap[2]]}</ion-button>
+            <ion-button class="item-text-wrap" id="item-3" fill="clear" on:click={selectAnswer(question.answerMap[3])}>{question[question.answerMap[3]]}</ion-button>
             </div>
         </ion-card>
 
     </ion-content>
+    <ion-footer>
+        <ion-toolbar>
+            <!-- <ion-buttons slot="start">
+                <ion-menu-button />
+            </ion-buttons> -->
+            {#if total > 0}
+            <ion-title>{correct} of {total} ({Math.round(correct / Math.max(1, total) * 100)}%)</ion-title>
+            {/if}
+        </ion-toolbar>
+    </ion-footer>
 </IonPage>
 <style></style>
