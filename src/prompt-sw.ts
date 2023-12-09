6070473eff4a01dd6e537b116f8c2329bf793ada
Mark Burggraf <markb@mantisbible.com>
2023-12-09 07:23:41 -0800

    fixed service worker

--- src/prompt-sw.ts
+++ src/prompt-sw.ts
@@ -25,15 +25,9 @@ let allowlist: undefined | RegExp[]
 if (import.meta.env.DEV)
   allowlist = [/^\/$/]
 
-  const adminPathRegExp = new RegExp('^\/_\/.*');
-
 // to allow work offline
 registerRoute(new NavigationRoute(
   createHandlerBoundToURL('/'),
-  // { allowlist, denylist:  [new RegExp('/api/')], },
   { allowlist, denylist:  [  /(_|api)(\/.*)?$/  ] },
-
-
-  // [  /(_|api)(\/.*)?$/  ]
-  ))
+))
 
